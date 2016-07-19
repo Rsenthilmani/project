6c9bd9a7aff860a6c6da39bff23b95c8d71d2008
Rsenthilmani <senthilanandh100@gmail.com>
2016-07-19 09:20:11 +0530

    Create README.java

---
+++ README.java
@@ -0,0 +1,25 @@
+class std
+{
+String s;
+public int display(int a)
+{
+String s=a;
+int b=s.length;
+if(int a<1)
+{
+return 0;
+}
+else 
+{
+return s;
+}
+}
+}
+class from
+{
+public static void main(String[] args)
+{
+std d=new std(239);
+system.out.println(d.display(29));
+}
+}
